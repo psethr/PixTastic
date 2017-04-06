363fa2f99e4e2cd487980be9dcb9f7c1e4bd445e
Seth <Seth@SETH-LAPTOP>
2017-04-06 12:23:46 -0400

    Formatted PixTastic Class

--- src/PixTastic.java
+++ src/PixTastic.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class PixTastic
+{
 
-/**
- *
- * @author Seth
- */
-public class PixTastic {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        
     }
     
 }
