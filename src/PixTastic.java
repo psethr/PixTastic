d26bafec06edefca60d4973ace6d2aa8adc0d7bf
Seth <Seth@SETH-LAPTOP>
2017-04-10 22:25:38 -0400

    Added test button

--- src/PixTastic.java
+++ src/PixTastic.java
@@ -1,18 +1,43 @@
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class PixTastic extends Application
 {
-
+    Stage window;
+    Button button;
+    
     public static void main(String[] args)
     {
         launch(args);
     }
     
     @Override
-    public void start(Stage primaryStage)
+    public void start(Stage window)
     {
-        primaryStage.setTitle("PixTastic");
+        this.window = window;
+        window.setTitle("PixTastic");
+        
+        button = new Button();
+        button.setText("Click Here");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event)
+                {
+                    System.out.println("Success");
+                }
+        });
+        
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(button);
+        
+        Scene scene = new Scene(layout, 300, 250);
+        
+        window.setScene(scene);
+        window.show();
     }
 }
