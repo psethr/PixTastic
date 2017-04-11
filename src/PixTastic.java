5d76ec7109b795a1ca8f838aa1b4c0e9ef1b2f2f
Seth <Seth@SETH-LAPTOP>
2017-04-10 21:23:18 -0400

    Created GUI window

--- src/PixTastic.java
+++ src/PixTastic.java
@@ -1,9 +1,18 @@
-public class PixTastic
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PixTastic extends Application
 {
 
     public static void main(String[] args)
     {
-        
+        launch(args);
     }
     
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("PixTastic");
+    }
 }
