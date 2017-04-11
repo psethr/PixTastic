079766513f9d7c1219cd9ac238e1e1ac17ca1fa4
Seth <Seth@SETH-LAPTOP>
2017-04-11 11:25:50 -0400

    Add authors

--- src/PixTastic.java
+++ src/PixTastic.java
@@ -1,3 +1,4 @@
+//Authors seth, rachel, tiffany
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -30,7 +31,6 @@ public class PixTastic extends Application
                 {
                     System.out.println("Success");
                     window.close();
-                    www.homeandlearn.chttps://www.google.com/maps/place/RunRite+Small+Engine+Repair+%26+Custom+Golf+Carts/@38.2391926,-77.5371852,14z/data=!4m20!1m14!4m13!1m6!1m2!1s0x89b6c7206508ecd7:0x41910e4d3fa86d19!2s9503+Whiteheart+Drive,+Fredericksburg,+VA+22407-9332,+USA!2m2!1d-77.51696!2d38.2172849!1m5!1m1!1s0x89b6c112422699af:0x2211eba75af5e666!2m2!1d-77.4997942!2d38.2641129!3m4!1s0x89b6c112422699af:0x2211eba75af5e666!8m2!3d38.2641129!4d-77.4997942o.uk/java/java_and_databases.html
                 }
         });
         
