dfd66308160898c293a3fe203333957e34b3fba2
Seth <Seth@SETH-LAPTOP>
2017-04-12 18:26:18 -0400

    Add GUI start page

--- src/PixTastic.java
+++ src/PixTastic.java
@@ -2,15 +2,22 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.*;
 
 public class PixTastic extends Application
 {
     Stage window;
     Button button;
+    Label name;
+    HBox topMenu;
+    HBox centerMenu;
+    BorderPane borderPane;
     
     public static void main(String[] args)
     {
@@ -23,8 +30,11 @@ public class PixTastic extends Application
         this.window = window;
         window.setTitle("PixTastic");
         
+        name = new Label("PixTastic");
+        name.setFont(Font.font("Permanent Marker", 80));
+        
         button = new Button();
-        button.setText("Click Here");
+        button.setText("Close Program");
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event)
@@ -33,11 +43,21 @@ public class PixTastic extends Application
                     window.close();
                 }
         });
+        Button button2 = new Button("Login");
+        button2.setFont(Font.font("Arial", 50));
+        topMenu = new HBox();
+        topMenu.setAlignment(Pos.CENTER);
+        topMenu.setPadding(new Insets(10));
+        topMenu.setSpacing(500.0);
+        topMenu.getChildren().addAll(name, button);
+        centerMenu = new HBox();
+        centerMenu.getChildren().addAll(button2);
+        centerMenu.setAlignment(Pos.CENTER);
+        borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setCenter(centerMenu);
         
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(button);
-        
-        Scene scene = new Scene(layout, 300, 250);
+        Scene scene = new Scene(borderPane, 1000, 900);
         
         window.setScene(scene);
         window.show();
