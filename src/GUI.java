0343a82a74c5afadfb20a430862a9be0fc2c091b
Seth <Seth@SETH-LAPTOP>
2017-04-23 14:31:00 -0400

    Added user class and picture class(fixed error)

--- src/GUI.java
+++ src/GUI.java
@@ -200,7 +200,7 @@ public class GUI
                                                     createAccountTextFieldProfilePic.getText(),
                                                     createAccountTextFieldBiography.getText(),
                                                     true,
-                                                    true)
+                                                    true);
             PixTastic.registeredUserAL.add(user);
             Profile(user);
         });
